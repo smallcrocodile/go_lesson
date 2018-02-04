a1beef3addf2b784f0d341c27a5d4a0f318ef055
erfeng.cheng <erfeng.cheng@dianping.com>
2018-02-04 14:04:43 +0800

    no message

--- basic/triangle_test.go
+++ basic/triangle_test.go
@@ -1,1 +1,17 @@
-package basic
+package main
+
+import "testing"
+
+func TestTriangle(t *testing.T)  {
+	tests :=[]struct{a,b,c int}{
+		{3,4,5},
+		{5,12,13},
+		{8,15,17}
+	}
+	for _,tt :=range tests{
+		if actual := calcTriangle(tt.a,tt.b);actual != tt.c{
+			t.Error("calctriangle(%d,%d); got %d;expected:%d",tt.a,tt.b,actual,tt.c)
+		}
+	}
+}
+
