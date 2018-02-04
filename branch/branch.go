a2e1f404c320c178104ba0f4d4f64db39fd71d17
erfeng.cheng <erfeng.cheng@dianping.com>
2018-02-04 14:04:48 +0800

    no message

--- branch/branch.go
+++ branch/branch.go
@@ -1,1 +1,37 @@
-package branch
+package main
+
+import (
+	"io/ioutil"
+	"fmt"
+)
+
+func main()  {
+	const filename = "abc.txt"
+	if contents,err :=ioutil.ReadFile(filename);err != nil {
+		fmt.Println(err)
+	}else {
+		fmt.Printf("%s\n",contents)
+	}
+
+	fmt.Println(grade(0),
+	grade(53),
+	grade(100),
+	grade(99))
+}
+
+func grade(score int)string  {
+	g := ""
+	switch  {
+	case score< 0||score >100:
+		panic(fmt.Sprintf("wrong score :%d",score))
+	case score < 60:
+		g = "F"
+	case score<70:
+		 g = "C"
+	case score<80:
+		g = "B"
+	case score<=100:
+		g = "A"
+	}
+	return g
+}
