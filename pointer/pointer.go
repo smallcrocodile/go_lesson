6b7ee52c6079d1c99cf3d6aeca7e9960d23e0423
erfeng.cheng <erfeng.cheng@dianping.com>
2018-02-05 16:44:48 +0800

    no message

--- pointer/pointer.go
+++ pointer/pointer.go
@@ -7,4 +7,22 @@ func main()  {
 	var pa *int = &a
 	*pa = 3
 	fmt.Println(a)
+
+	var  ptr *int
+	fmt.Printf("ptr 的值为 : %x\n", ptr  )
+	a,b := 3,4
+	swap(&a,&b)
+	fmt.Println(a,b)
+	a,b = swap1(a,b)
+	fmt.Println(a,b)
+
+
+}
+
+func swap(a,b *int)  {
+	*b,*a = *a,*b
+}
+
+func swap1(a,b int)(int,int)  {
+	return b,a
 }
