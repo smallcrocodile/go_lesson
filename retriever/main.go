86b5dcc5fa79209724b6f51ef1be302351da1390
erfeng.cheng <erfeng.cheng@dianping.com>
2018-02-10 20:29:56 +0800

    接口

--- retriever/main.go
+++ retriever/main.go
@@ -1,1 +1,47 @@
-package retriever
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"imooc/retriever/mock"
+	"imooc/retriever/real"
+)
+
+type Retriever interface {
+	Get(url string) string
+}
+
+func download(r Retriever) string {
+	return r.Get("http://www.imooc.com")
+}
+
+func main() {
+	var r Retriever
+	r = mock.Retriever{"fake url"}
+	inspect(r)
+	r = &real.Retriever{UserAgent: "Mozile/5.0", TimeOut: time.Minute}
+	inspect(r)
+	//fmt.Printf(download(r))
+
+	//Type assertion
+	if mockRetriver, ok := r.(*mock.Retriever); ok {
+		fmt.Println(mockRetriver.Contents)
+	} else {
+		fmt.Println("not mock retriver")
+
+	}
+
+}
+
+func inspect(r Retriever) {
+	fmt.Printf("%T %v\n", r, r)
+	fmt.Println(r)
+	switch v := r.(type) {
+	case mock.Retriever:
+		fmt.Println("contents:", v.Contents)
+	case *real.Retriever:
+		fmt.Println("UserAgent:", v.UserAgent)
+
+	}
+}
--- retriever/real/retriever.go
+++ retriever/real/retriever.go
@@ -1,1 +1,25 @@
-package retriever
+package real
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"time"
+)
+
+type Retriever struct {
+	UserAgent string
+	TimeOut   time.Duration
+}
+
+func (r *Retriever) Get(url string) string {
+	rep, err := http.Get(url)
+	if err != nil {
+		panic(err)
+	}
+	result, err := httputil.DumpResponse(rep, true)
+	rep.Body.Close()
+	if err != nil {
+		panic(err)
+	}
+	return string(result)
+}
