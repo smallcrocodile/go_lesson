e317123e7d96deb118c9a6139ea7277631f15037
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-02 16:02:55 +0800

    no message

--- errorhandler/handler/handler.go
+++ errorhandler/handler/handler.go
@@ -1,1 +1,39 @@
 package handler
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+)
+
+const prefix = "/list/"
+
+func HandleFileList(writer http.ResponseWriter, request *http.Request) error {
+	fmt.Println(request.URL.Path)
+	if !strings.HasPrefix(request.URL.Path, prefix) {
+		return userError(fmt.Sprintf("path %s must start with %s", request.URL.Path, prefix))
+	}
+	path := request.URL.Path[len("/list/"):]
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	all, err := ioutil.ReadAll(file)
+	if err != nil {
+		return err
+	}
+	writer.Write(all)
+	return nil
+}
+
+type userError string
+
+func (e userError) Error() string {
+	return e.Message()
+}
+func (e userError) Message() string {
+	return string(e)
+}
