33ce713d773a368545527821dd89bedb8a547bab
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-02 16:02:28 +0800

    no message

--- basic/triangle_test.go
+++ basic/triangle_test.go
@@ -2,16 +2,16 @@ package main
 
 import "testing"
 
-func TestTriangle(t *testing.T)  {
-	tests :=[]struct{a,b,c int}{
-		{3,4,5},
-		{5,12,13},
-		{8,15,17}
+func TestTriangle(t *testing.T) {
+	tests := []struct{ a, b, c int }{
+		{3, 4, 5},
+		{5, 12, 13},
+		{8, 15, 17},
+		{30000, 40000, 50000},
 	}
-	for _,tt :=range tests{
-		if actual := calcTriangle(tt.a,tt.b);actual != tt.c{
-			t.Error("calctriangle(%d,%d); got %d;expected:%d",tt.a,tt.b,actual,tt.c)
+	for _, tt := range tests {
+		if actual := calcTriangle(tt.a, tt.b); actual != tt.c {
+			t.Errorf("calctriangle(%d,%d); got %d;expected:%d", tt.a, tt.b, actual, tt.c)
 		}
 	}
 }
-
