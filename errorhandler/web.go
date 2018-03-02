15e9713e182b363335007d85984c6cad96548e9a
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-02 16:02:59 +0800

    no message

--- errorhandler/web.go
+++ errorhandler/web.go
@@ -1,1 +1,54 @@
-package errorhandler
+package main
+
+import (
+	"imooc/errorhandler/handler"
+	"log"
+	"net/http"
+	"os"
+)
+
+type appHandler func(writer http.ResponseWriter, request *http.Request) error
+type userError interface {
+	error
+	Message() string
+}
+
+func errorWrapper(handlerrr appHandler) func(w http.ResponseWriter, r *http.Request) {
+	return func(writer http.ResponseWriter, request *http.Request) {
+		defer func() {
+			r := recover()
+			if r != nil {
+				log.Printf("Panic:%v", r)
+				http.Error(writer, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			}
+		}()
+		err := handlerrr(writer, request)
+		if err != nil {
+			log.Printf("Error handling request: %s", err.Error())
+			if userErr, ok := err.(userError); ok {
+				http.Error(writer, userErr.Message(), http.StatusBadRequest)
+				return
+			}
+
+			code := http.StatusOK
+			switch {
+			case os.IsNotExist(err):
+				code = http.StatusNotFound
+			case os.IsPermission(err):
+				code = http.StatusForbidden
+			default:
+				code = http.StatusInternalServerError
+			}
+			http.Error(writer, http.StatusText(code), code)
+		}
+
+	}
+}
+
+func main() {
+	http.HandleFunc("/", errorWrapper(handler.HandleFileList))
+	err := http.ListenAndServe(":8888", nil)
+	if err != nil {
+		panic(err)
+	}
+}
