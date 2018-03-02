e7efb3611277bd183b44cf0c4b2fbab37dad0b4f
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-02 16:02:50 +0800

    no message

--- errorhandler/recover/recover.go
+++ errorhandler/recover/recover.go
@@ -1,1 +1,27 @@
-package recover
+package main
+
+import (
+	"fmt"
+	"log"
+)
+
+func tryRecover() {
+	defer func() {
+		r := recover()
+		if err, ok := r.(error); ok {
+			log.Println("Error occured:", err)
+		} else {
+			panic(fmt.Sprintf("I don't know what to do:%v", r))
+		}
+	}()
+
+	//panic(errors.New("this is an error"))
+	//b := 0
+	//a := 5 / b
+	//fmt.Println(a)
+	//panic(922)
+}
+
+func main() {
+	tryRecover()
+}
