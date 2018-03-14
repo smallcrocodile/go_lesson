96157e7e52196281987e7378f72ae2b2980d696d
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-14 16:24:19 +0800

    正则表达式

--- basic/regex/regex.go
+++ basic/regex/regex.go
@@ -1,1 +1,16 @@
-package regex
+package main
+
+import (
+	"fmt"
+	"regexp"
+)
+
+const text = "My email is jack@gmail.com  " +
+	"My email is aa@gmail.com" +
+	"My email is jack@gmail.com.com"
+
+func main() {
+	re := regexp.MustCompile("([a-zA-Z0-9]+)@([a-zA-Z0-9]+)\\.([a-zA-Z0-9.]+)")
+	amtch := re.FindAllStringSubmatch(text, -1)
+	fmt.Println(amtch)
+}
