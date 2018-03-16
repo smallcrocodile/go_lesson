98054d82f04a9c5efc267d5189c3ce81010f5d3a
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-16 18:48:12 +0800

    run 修改

--- craw/engine/concurrent.go
+++ craw/engine/concurrent.go
@@ -1,1 +1,50 @@
 package engine
+
+import "log"
+
+type ConcurrentEngine struct {
+	Scheduler   Scheduler
+	WorkerCount int
+}
+
+type Scheduler interface {
+	Submit(Request)
+	ConfigureMasterWorkerChan(chan Request)
+}
+
+func (e *ConcurrentEngine) Run(seeds ...Request) {
+	in := make(chan Request)
+	out := make(chan ParseResult)
+	e.Scheduler.ConfigureMasterWorkerChan(in)
+	for i := 0; i < e.WorkerCount; i++ {
+		createWoker(in, out)
+	}
+
+	for _, r := range seeds {
+		e.Scheduler.Submit(r)
+	}
+	itemCount := 0
+	for {
+		result := <-out
+		for _, item := range result.Items {
+			itemCount++
+			log.Printf("Got item #%d: %v", itemCount, item)
+		}
+		for _, request := range result.Requests {
+			e.Scheduler.Submit(request)
+		}
+	}
+}
+
+func createWoker(in chan Request, out chan ParseResult) {
+	go func() {
+		for {
+			request := <-in
+			result, err := worker(request)
+			if err != nil {
+				continue
+			}
+			out <- result
+		}
+	}()
+}
