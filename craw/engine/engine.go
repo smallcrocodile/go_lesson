14b5baf7efe525561b835ba5a288b02fe6a4f96d
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-16 16:03:08 +0800

    引擎

--- craw/engine/engine.go
+++ craw/engine/engine.go
@@ -1,1 +1,29 @@
 package engine
+
+import (
+	"imooc/craw/fetcher"
+
+	"log"
+)
+
+func Run(seeds ...Request) {
+	var requests []Request
+	for _, r := range seeds {
+		requests = append(requests, r)
+	}
+	for len(requests) > 0 {
+		r := requests[0]
+		requests = requests[1:]
+		log.Println("Fetching ", r.Url)
+		body, err := fetcher.Fetch(r.Url)
+		if err != nil {
+			log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
+			continue
+		}
+		parseResult := r.ParaserFunc(body)
+		requests = append(requests, parseResult.Requests...)
+		for _, item := range parseResult.Items {
+			log.Printf("Got Item %v", item)
+		}
+	}
+}
