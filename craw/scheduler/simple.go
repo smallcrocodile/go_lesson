bfd8a3b289cce329810bcc9692d8b965e26475e4
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-16 18:48:25 +0800

    简单调度器

--- craw/scheduler/simple.go
+++ craw/scheduler/simple.go
@@ -1,1 +1,19 @@
 package scheduler
+
+import "imooc/craw/engine"
+
+type SimpleScheduler struct {
+	workerChan chan engine.Request
+}
+
+func (s *SimpleScheduler) ConfigureMasterWorkerChan(c chan engine.Request) {
+	s.workerChan = c
+}
+
+func (s *SimpleScheduler) Submit(request engine.Request) {
+	//send request down to woker chan
+	go func() {
+		s.workerChan <- request
+	}()
+
+}
