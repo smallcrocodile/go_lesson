b33e2f2f0fd6aed7d1028feb5daffddf5db4b2c9
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-16 16:03:19 +0800

    no message

--- craw/engine/types.go
+++ craw/engine/types.go
@@ -1,1 +1,15 @@
 package engine
+
+type Request struct {
+	Url         string
+	ParaserFunc func([]byte) ParseResult
+}
+
+type ParseResult struct {
+	Requests []Request
+	Items    []interface{}
+}
+
+func NilParser([]byte) ParseResult {
+	return ParseResult{}
+}
