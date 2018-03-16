007ad1e48f0db726b045b155fb8ea4e38a659d52
erfeng.cheng <erfeng.cheng@dianping.com>
2018-03-16 18:47:51 +0800

    并发调度

--- craw/engine/simple_engine.go
+++ craw/engine/simple_engine.go
@@ -14,16 +14,24 @@ func Run(seeds ...Request) {
 	for len(requests) > 0 {
 		r := requests[0]
 		requests = requests[1:]
-		log.Println("Fetching ", r.Url)
-		body, err := fetcher.Fetch(r.Url)
+
+		parseResult, err := worker(r)
 		if err != nil {
-			log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
 			continue
 		}
-		parseResult := r.ParaserFunc(body)
 		requests = append(requests, parseResult.Requests...)
 		for _, item := range parseResult.Items {
 			log.Printf("Got Item %v", item)
 		}
 	}
+}
+
+func worker(r Request) (ParseResult, error) {
+	log.Println("Fetching ", r.Url)
+	body, err := fetcher.Fetch(r.Url)
+	if err != nil {
+		log.Printf("Fetcher: error fetching url %s: %v", r.Url, err)
+		return ParseResult{}, err
+	}
+	return r.ParaserFunc(body), nil
 }
